c6ad1023d53318ee4d70349e4a6de08dab4d507f
Andrew Kyanjo <59293622+AndrewKyanjo@users.noreply.github.com>
2026-04-08 13:45:19 +0300

    feat: add EventItem type and sample events data to constants

---
+++ lib/constants.ts
@@ -0,0 +1,87 @@
+export type EventItem = {
+    title: string;
+    image: string;
+    date: string;
+    location: string;
+    time: string;
+};
+
+export const events: EventItem[] = [
+    {
+        image: "/images/event1.png",
+        title: "JS Conference 2024",
+        date: "2024-07-01",
+        location: "Kampala",
+        time: "10:00 AM - 4:00 PM",
+    },
+    {
+        image: "/images/event2.png",
+        title: "React Summit 2024",
+        date: "2024-07-02",
+        location: "Nairobi",
+        time: "10:00 AM - 4:00 PM",
+    },
+    {
+        image: "/images/event3.png",
+        title: "Node.js Global Summit 2024",
+        date: "2024-07-03",
+        location: "Arusha",
+        time: "10:00 AM - 4:00 PM",
+    },
+    {
+        image: "/images/event4.png",
+        title: "Frontend Masters 2024",
+        date: "2024-07-04",
+        location: "Mombasa",
+        time: "10:00 AM - 4:00PM",
+    },
+    {
+        image: "/images/event5.png",
+        title: "Full Stack Fest 2024",
+        date: "2024-07-05",
+        location: "Dar es Salaam",
+        time: "10:00 AM - 4:00 PM",
+    },
+    {
+        image: "/images/event6.png",
+        title: "AI & Machine Learning Summit 2024",
+        date: "2024-07-06",
+        location: "Accra",
+        time: "9:00 AM - 3:00 PM",
+    },
+    {
+        image: "/images/event7.jpg",
+        title: "Cloud Computing Expo 2024",
+        date: "2024-07-07",
+        location: "Lagos",
+        time: "8:00 AM - 1:00 PM",
+    },
+    {
+        image: "/images/event8.jpg",
+        title: "Cybersecurity Conference 2024",
+        date: "2024-07-08",
+        location: "Johannesburg",
+        time: "11:00 AM - 3:00 PM",
+    },
+    {
+        image: "/images/event9.jpg",
+        title: "Data Science Summit 2024",
+        date: "2024-07-09",
+        location: "Kigali",
+        time: "10:00 AM - 4:00 PM",
+    },
+    {
+        image: "/images/event10.jpg",
+        title: "Mobile Development Conference 2024",
+        date: "2024-07-10",
+        location: "Dakar",
+        time: "10:00 AM - 4:00 PM",
+    },
+    {
+        image: "/images/event.jpg",
+        title: "DevOps & SRE Summit 2024",
+        date: "2024-07-10",
+        location: "Cairo",
+        time: "12:01 PM - 8:00 PM",
+    },
+];
